564063e505fb1ab6a56ecf1719cda554f5bdd197
yuhanghong123 <3100659858@qq.com>
2024-06-29 21:39:49 +0800

    update Res18model

--- Res18/modules.py
+++ Res18/modules.py
@@ -305,14 +305,14 @@ class ResDeconv(nn.Module):
         img = self.deconv(features)
         return img
 
-def resnet18(pretrained=False, **kwargs):
-    """Constructs a ResNet-18 model.
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-    """
-    model = ResNet(BasicBlock, [2, 2, 2, 2], **kwargs)
-    if pretrained:
-        model.load_state_dict(model_zoo.load_url(model_urls['resnet18']),strict=False)
-    return model
+# def resnet18(pretrained=False, **kwargs):
+#     """Constructs a ResNet-18 model.
+#     Args:
+#         pretrained (bool): If True, returns a model pre-trained on ImageNet
+#     """
+#     model = ResNet(BasicBlock, [2, 2, 2, 2], **kwargs)
+#     if pretrained:
+#         model.load_state_dict(model_zoo.load_url(model_urls['resnet18']),strict=False)
+#     return model
 
 
