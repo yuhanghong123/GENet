f030b5c764c33cfbd9a0f1738de187f49f411965
YuhangHong <115858438+yuhanghong123@users.noreply.github.com>
2024-08-03 16:06:51 +0800

    Delete VMamba directory

--- VMamba/VMamba_model.py
+++
@@ -1,9 +0,0 @@
-from Vmamba_modules import VSSM
-import torch 
-
-if __name__ == '__main__':
-    # 创建一个VSSM对象
-    vssm = VSSM()
-    input = torch.randn(1, 3, 224, 224)
-    output = vssm(input)
-    print(output.shape)
