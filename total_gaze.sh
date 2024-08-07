2d440aa1ebfbdbd0751c6c7e0b042a1dab09f83d
YuhangHong <115858438+yuhanghong123@users.noreply.github.com>
2024-08-07 15:09:47 +0800

    Update total_gaze.sh

--- total_gaze.sh
+++ total_gaze.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# 运行 Python 脚本 total_gaze360.py 并传递参数
+
 python trainer.py -c config/train/config_gaze360.yaml
 python total_gaze360.py -s config/train/config_gaze360.yaml -t config/test/config_gaze360_diap.yaml
 python total_gaze360.py -s config/train/config_gaze360.yaml -t config/test/config_gaze360_mpii.yaml
